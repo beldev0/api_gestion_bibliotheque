4b0d65f935de0d8e4f4cb5bb00de3fd08dbfd8e0
= <=>
2026-03-22 21:35:35 +0100

    Redefinition de la méthode rename_file pour carte etudiant

--- src/members/models.py
+++ src/members/models.py
@@ -11,6 +11,10 @@ def rename_file(self, filename):
     ext = filename.split('.')[-1]
     return f"profil/{uuid.uuid4()}.{ext}"
 
+def rename_etudiant_carte_file(self, filename):
+    ext = filename.split('.')[-1]
+    return f"carte/{uuid.uuid4()}.{ext}"
+
 
 class CustomManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -40,22 +44,32 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=10)
-    address = models.CharField(max_length=255, blank=True)
-    carte_etudiant = models.ImageField(blank=True, null=True, upload_to=rename_file)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    carte_etudiant = models.ImageField(blank=True, null=True, upload_to=rename_etudiant_carte_file)
     role = models.CharField(choices=[
         ('manager', 'Manager'),
         ('librarian', 'Librarian'),
         ('member', 'Member')
-    ], max_length=9)
+    ], default='member', max_length=9)
     birthday = models.DateField()
     status = models.CharField(choices=[
         ('actif', 'Actif'),
         ('suspendu', 'Suspendu'),
         ('bloque', 'Bloqué'),
         ('expire', 'Expiré')
-    ], max_length=8)
+        ('attente', 'En attente')
+    ], default="actif", max_length=8)
+    profil = models.ImageField(upload_to=rename_file, blank=True, default=None, null=True),
+
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomManager()
+
+
+class InvitationToken(models.Model):
+    jti = models.CharField(max_length=255)
+    is_used = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+    

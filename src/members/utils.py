0037fc0afd20f8d3bd7dc1790ebf352a3ac34137
= <=>
2026-03-22 21:36:29 +0100

    Fonctions utilitaires pour gérer la validation des tokens et géneration des tokens d'invitation

---
+++ src/members/utils.py
@@ -0,0 +1,36 @@
+from rest_framework_simplejwt.tokens import Token
+from rest_framework_simplejwt.exceptions import TokenError
+from  datetime import timedelta
+from .models import InvitationToken
+
+class InvitationTokenClass(Token):
+    lifetime = timedelta(hours=24)
+    token_type = "Invitation"
+
+def createInvitationToken(email):
+    token = InvitationTokenClass()
+    token["email"] = email
+    jti = token['jti']
+
+    InvitationToken.objects.create(jti=jti, email=email)
+
+    return str(token)
+
+def isValidToken(token_string):
+    try:
+        token = InvitationTokenClass(token_string)
+        jti = token['jti']
+
+        try:
+            invitation = InvitationToken.objects.get(jti=jti)
+        except InvitationToken.DoesNotExist:
+            return None, "Token non reconnu"
+        
+        if invitation.is_used:
+            return None, "Token d'invitation expiré"
+        
+        return jti, None
+    except TokenError as e:
+        return None, str(e)
+
+

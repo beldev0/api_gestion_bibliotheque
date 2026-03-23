cc6d0b96eab65779daf554e6a7cae147c1555034
= <=>
2026-03-23 12:07:46 +0100

    Ajout des vues de changePassword, me, allusers

--- src/members/views.py
+++ src/members/views.py
@@ -1,8 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from .serializers import CustomUserSerializer
+from .serializers import *
 from .utils import get_token_for_user
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 @api_view(['POST'])
 def register(request):
@@ -16,3 +21,40 @@ def register(request):
             **tokens
         }, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def changePassword(request):
+    serializer = changePasswordSerializer(data=request.data, context={'request':request})
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Mot de passe modifié avec succès"} ,status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'PATCH'])
+def editUserProfil(request):
+    user = request.user
+
+    if request.method ==  "PUT":
+        serializer = UserProfileSerializer(user, data=request.data)
+    else:
+        serializer = UserProfileSerializer(user, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def me(request):
+    user_id = request.user.id
+
+    user = get_object_or_404(User, id=user_id)
+    serializer = UserProfileSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def allUsers(request):
+    users = User.objects.all()
+    serializer = UserProfileSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
